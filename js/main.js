d83aad0ad09bd8f5a8c7ffa5a7d2bad2ed6fbfc5
Zhenya <zenamalkov1@gmail.com>
2022-06-14 19:09:33 +0300

    save

--- js/main.js
+++ js/main.js
@@ -3,4 +3,24 @@ $(document).ready(function(){
     dots:true
 });
 });
-highAndLow("1 2 3 4 5");
+document.addEventListener("DOMContentLoaded",()=>{
+   let user_email=document.querySelector(".newsletter-user-email");
+   let warning=document.querySelector(".email__warning")
+   console.log(warning)
+   user_email.addEventListener("blur",()=>{
+      var re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+      var valid = re.test(user_email.value);
+      if(valid){
+        warning.style.display="none";
+        user_email.style.border=`1px solid rgba(0,0,0,0.02)`
+      }
+      else{
+        warning.style.display="block";
+        user_email.style.border=`2px solid rgb(232, 77, 77)`
+      };
+   })
+
+   
+   
+
+});
