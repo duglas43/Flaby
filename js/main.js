0154d7c255d34fe641a94c22b6a9ad856c41c74c
Zhenya <zenamalkov1@gmail.com>
2022-06-15 06:07:03 +0300

    new burger

--- js/main.js
+++ js/main.js
@@ -18,12 +18,15 @@ document.addEventListener("DOMContentLoaded",()=>{
         user_email.style.border=`2px solid rgb(232, 77, 77)`
       };
    })
-   const burger_btn=document.querySelector(".burger__image");
+   const burger_btn=document.querySelector(".burger");
    const burger_menu=document.querySelector(".burger-menu");
    const header=document.querySelector("header");
+   const intro=document.querySelector(".intro");
    burger_btn.addEventListener("click",()=>{
-    burger_menu.classList.toggle("open");
-    header.classList.toggle("hide__color");
-    document.body.classList.toggle("slide_prevent")
+      burger_menu.classList.toggle("open");
+      header.classList.toggle("hide__color");
+      document.body.classList.toggle("slide_prevent");
+      burger_btn.classList.toggle("burger_active");
+      intro.classList.toggle("hide");
    })
 });
